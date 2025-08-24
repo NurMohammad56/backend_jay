f1d14f55ba0e85458798e2d2a083413891c54917
NUR MOHAMMAD <nurmohammad0605@gmail.com>
2025-08-24 16:39:18 +0600

    feat: Add user settings retrieval and update functionality

---
+++ controller/admin.controller.js
@@ -0,0 +1,97 @@
+import { Report } from "../model/report.model.js";
+import { User } from "../model/user.model.js";
+import sendResponse from "../utils/sendResponse.js";
+import catchAsync from "../utils/catchAsync.js";
+import AppError from "../errors/AppError.js";
+
+export const getUsers = catchAsync(async (req, res) => {
+  const users = await User.find().select("-__v");
+  sendResponse(res, {
+    statusCode: 200,
+    success: true,
+    message: "Users retrieved successfully",
+    data: users,
+  });
+});
+
+export const deleteUser = catchAsync(async (req, res) => {
+  const { id } = req.params;
+  await User.findByIdAndDelete(id);
+  await Report.deleteMany({ userId: id });
+  sendResponse(res, {
+    statusCode: 200,
+    success: true,
+    message: "User deleted successfully",
+  });
+});
+
+export const getMonthlyStats = catchAsync(async (req, res) => {
+  let { year = new Date().getFullYear(), type, month } = req.query;
+  year = parseInt(year);
+  month = month ? parseInt(month) : null;
+
+  let match = {
+    createdAt: {
+      $gte: new Date(`${year}-01-01`),
+      $lt: new Date(`${year + 1}-01-01`),
+    },
+  };
+
+  if (type) {
+    match.category = type;
+  }
+
+  if (month) {
+    const startOfMonth = new Date(year, month - 1, 1);
+    const endOfMonth =
+      month === 12 ? new Date(year + 1, 0, 1) : new Date(year, month, 1);
+
+    match.createdAt.$gte = startOfMonth;
+    match.createdAt.$lt = endOfMonth;
+  }
+
+  const stats = await Report.aggregate([
+    { $match: match },
+    {
+      $group: {
+        _id: { month: { $month: "$createdAt" }, category: "$category" },
+        count: { $sum: 1 },
+      },
+    },
+    { $sort: { "_id.month": 1, count: -1 } },
+  ]);
+
+  if (type || month) {
+    const counts = {};
+    stats.forEach((stat) => {
+      const m = stat._id.month;
+      if (!counts[m]) counts[m] = {};
+      counts[m][stat._id.category] = stat.count;
+    });
+
+    return sendResponse(res, {
+      statusCode: 200,
+      success: true,
+      message: "Monthly report counts retrieved successfully",
+      data: counts,
+    });
+  }
+
+  const highestPerMonth = {};
+  stats.forEach((stat) => {
+    const m = stat._id.month;
+    if (!highestPerMonth[m] || stat.count > highestPerMonth[m].count) {
+      highestPerMonth[m] = {
+        category: stat._id.category,
+        count: stat.count,
+      };
+    }
+  });
+
+  return sendResponse(res, {
+    statusCode: 200,
+    success: true,
+    message: "Monthly report highest alert rates retrieved successfully",
+    data: highestPerMonth,
+  });
+});
